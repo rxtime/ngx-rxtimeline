0a7c149978301289cc8f64a5c15a21bfbdae3d6e
Umar Mohammed <umar@teacherapp.co.uk>
2020-02-09 16:53:29 +0000

    remove generated library files
    closes #19

--- projects/ngx-d3timeline/src/lib/ngx-d3timeline.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class NgxD3timelineService {
-
-  constructor() { }
-}
--- projects/ngx-d3timeline/src/public-api.ts
+++ projects/ngx-d3timeline/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-d3timeline
  */
 
-export * from './lib/ngx-d3timeline.service';
 export * from './lib/ngx-d3timeline.component';
 export * from './lib/ngx-d3timeline.module';
