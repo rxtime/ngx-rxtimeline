23d5cb83d6a985d4080ce8f811ecc06929e16a23
Umar Mohammed <umar@teacherapp.co.uk>
2020-02-09 23:12:24 +0000

    bind to data property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { data } from './data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  data = data;
+}
