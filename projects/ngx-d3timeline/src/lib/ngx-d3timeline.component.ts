a9391c300d7243f42866f7ae7bbf9a45e4f6abf6
umarmohammed <umar@thinkincode.co.uk>
2020-02-10 22:01:04 +0000

    set svg dimensions (#32)
    * set svg dimensions
    
    closes #31
    
    * fix lint errors

--- projects/ngx-d3timeline/src/lib/ngx-d3timeline.component.ts
+++ projects/ngx-d3timeline/src/lib/ngx-d3timeline.component.ts
@@ -1,13 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { TimelineEvent } from './timeline-event';
+import { View } from './view';
 
 @Component({
   selector: 'ngx-d3timeline',
   template: `
-    <svg></svg>
+    <svg [attr.width]="view.width" [attr.height]="view.height"></svg>
   `,
   styles: []
 })
 export class NgxD3timelineComponent {
   @Input() data: TimelineEvent[];
+
+  view: View = {
+    width: 400,
+    height: 600,
+    margin: 50
+  };
 }
