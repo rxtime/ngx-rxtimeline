0c75b0494637973c91fe0439cef376b21b28bf0b
Umar Mohammed <umohammed@caci.co.uk>
2020-02-12 19:07:13 +0000

    rename bandscale to scaleBand

--- projects/ngx-d3timeline/src/lib/axis/serie-axis-view-model.ts
+++ projects/ngx-d3timeline/src/lib/axis/serie-axis-view-model.ts
@@ -3,35 +3,35 @@ import { TimelineEvent } from '../timeline-event';
 import { View } from '../view/view';
 
 export class SeriesAxisViewModel {
-  private readonly bandscale: ScaleBand<string>;
+  private readonly scaleBand: ScaleBand<string>;
 
   constructor(data: TimelineEvent[], view: View) {
-    this.bandscale = this.createBandscale(data, view);
+    this.scaleBand = this.configureScaleBand(data, view);
   }
 
   get tickInfos(): { label: string; transform: string }[] {
-    return this.bandscale.domain().map(tick => ({
+    return this.scaleBand.domain().map(tick => ({
       label: tick,
       transform: this.tickTransform(tick)
     }));
   }
 
   get rangeLimit() {
-    return this.bandscale.range()[1];
+    return this.scaleBand.range()[1];
   }
 
   get bandwidth() {
-    return this.bandscale.bandwidth();
+    return this.scaleBand.bandwidth();
   }
 
-  private createBandscale(data: TimelineEvent[], view: View) {
+  private configureScaleBand(data: TimelineEvent[], view: View) {
     return scaleBand()
       .domain([...new Set(data.map(d => d.series))])
       .range([0, view.width - view.margin]);
   }
 
   private tickTransform(tick: string) {
-    return `translate(${this.bandscale(tick) +
-      this.bandscale.bandwidth() / 2}, 0)`;
+    return `translate(${this.scaleBand(tick) +
+      this.scaleBand.bandwidth() / 2}, 0)`;
   }
 }
