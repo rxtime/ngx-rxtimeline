e38ea33ecfd67b232f30491fdac80b925fe613d4
Umar Mohammed <umar@teacherapp.co.uk>
2020-02-16 19:15:54 +0000

    move datasubject into seperate service

--- projects/ngx-d3timeline/src/lib/data.service.ts
+++ projects/ngx-d3timeline/src/lib/data.service.ts
@@ -1,6 +1,8 @@
 import { BehaviorSubject } from 'rxjs';
 import { TimelineEvent } from '../public-api';
+import { Injectable } from '@angular/core';
 
+@Injectable({ providedIn: 'root' })
 export class DataService {
   private dataSubject = new BehaviorSubject<TimelineEvent[]>(null);
 
