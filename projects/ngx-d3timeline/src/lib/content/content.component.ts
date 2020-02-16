9254478c28c4bb13545b8c4eb6dd1496b3fda737
Wade Fleming <wadefleming2000@hotmail.com>
2020-02-16 17:53:40 +0000

    added tests for ContentComponent

--- projects/ngx-d3timeline/src/lib/content/content.component.ts
+++ projects/ngx-d3timeline/src/lib/content/content.component.ts
@@ -4,17 +4,19 @@ import { ContentViewModel } from './content-view-model';
 @Component({
   selector: '[ngx-d3timeline-content]',
   template: `
-    <svg:g
+    <svg:g class="content-group" *ngIf="vm">
+      <svg:g
         *ngFor="let event of vm.data"
         [attr.transform]="vm.dataTransform(event)"
-    >
-      <rect
-        [attr.height]="vm.rectHeight(event)"
-        [attr.width]="vm.bandwidth"
-        fill="none"
-        stroke="#000"
-      ></rect>
-      <text font-size="10px" dy="1em">{{ event.type }}</text>
+      >
+        <rect
+          [attr.height]="vm.rectHeight(event)"
+          [attr.width]="vm.bandwidth"
+          fill="none"
+          stroke="#000"
+        ></rect>
+        <text font-size="10px" dy="1em">{{ event.type }}</text>
+      </svg:g>
     </svg:g>
   `
 })
