76675794063fd9b02efc6f47e4f06873a0b8b812
Umar Mohammed <umar@teacherapp.co.uk>
2020-02-29 11:55:56 +0000

    import actions as single variable

--- projects/ngx-d3timeline/src/lib/ngx-d3timeline.component.ts
+++ projects/ngx-d3timeline/src/lib/ngx-d3timeline.component.ts
@@ -10,12 +10,7 @@ import { zoom } from 'd3-zoom';
 import { select, event } from 'd3-selection';
 import { Orientation } from './orientation';
 import { Store } from './store/store';
-import {
-  DataChangedAction,
-  ViewChangedAction,
-  OrientationChangedAction,
-  ZoomEventAction
-} from './store/actions';
+import * as fromActions from './store/actions';
 
 @Component({
   selector: 'ngx-d3timeline',
@@ -36,15 +31,15 @@ import {
 })
 export class NgxD3timelineComponent implements AfterViewInit {
   @Input() set data(value: TimelineEvent[]) {
-    this.store.dispatch(new DataChangedAction(value));
+    this.store.dispatch(new fromActions.DataChangedAction(value));
   }
 
   @Input() set view([width, height]: [number, number]) {
-    this.store.dispatch(new ViewChangedAction([width, height]));
+    this.store.dispatch(new fromActions.ViewChangedAction([width, height]));
   }
 
   @Input() set orientation(value: Orientation) {
-    this.store.dispatch(new OrientationChangedAction(value));
+    this.store.dispatch(new fromActions.OrientationChangedAction(value));
   }
 
   @ViewChild('svgEl') svgEl: ElementRef;
@@ -54,7 +49,7 @@ export class NgxD3timelineComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     if (this.svgEl) {
       const onZoom = zoom().on('zoom', () =>
-        this.store.dispatch(new ZoomEventAction(event))
+        this.store.dispatch(new fromActions.ZoomEventAction(event))
       );
       onZoom(select(this.svgEl.nativeElement));
     }
