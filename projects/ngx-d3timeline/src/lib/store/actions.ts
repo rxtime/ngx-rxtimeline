510a86a5c7e04a20cbbac02697a21160130ff91d
Umar Mohammed <umar@teacherapp.co.uk>
2020-03-01 22:25:47 +0000

    add timeline event drag actions

--- projects/ngx-d3timeline/src/lib/store/actions.ts
+++ projects/ngx-d3timeline/src/lib/store/actions.ts
@@ -1,5 +1,7 @@
 import { TimelineEvent } from '../timeline-event';
 import { Orientation } from '../orientation';
+import { identifier } from '../types';
+import { EventRectangle } from '../content/content';
 
 export interface Action {
   type: string;
@@ -10,7 +12,10 @@ export enum ActionType {
   DataChanged = 'Data Changed',
   OrientationChanged = 'Orientation Changed',
   ViewChanged = 'View Changed',
-  Zoomed = 'Zoomed'
+  Zoomed = 'Zoomed',
+  TimelineDragStart = 'Timeline Drag Start',
+  TimelineDrag = 'Timeline Drag',
+  TimelineDragEnd = 'Timeline Drag End'
 }
 
 export class DataChangedAction implements Action {
@@ -33,8 +38,26 @@ export class ZoomedAction implements Action {
   constructor(public payload: any) {}
 }
 
+export class TimelineDragStartAction implements Action {
+  readonly type = ActionType.TimelineDragStart;
+  constructor(public payload: identifier) {}
+}
+
+export class TimelineDragAction implements Action {
+  readonly type = ActionType.TimelineDrag;
+  constructor(public payload: { eventRectangle: EventRectangle; event: any }) {}
+}
+
+export class TimelineDragEndAction implements Action {
+  readonly type = ActionType.TimelineDragEnd;
+  constructor(public payload: identifier) {}
+}
+
 export type Actions =
   | DataChangedAction
   | OrientationChangedAction
   | ViewChangedAction
-  | ZoomedAction;
+  | ZoomedAction
+  | TimelineDragStartAction
+  | TimelineDragAction
+  | TimelineDragEndAction;
