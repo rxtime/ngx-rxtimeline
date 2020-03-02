3c207b9e9435a58182e84cf11a987fd72814c482
Umar Mohammed <umar@teacherapp.co.uk>
2020-03-02 21:54:40 +0000

    fix linting errors in scale-utils

--- projects/ngx-d3timeline/src/lib/scale-util.ts
+++ projects/ngx-d3timeline/src/lib/scale-util.ts
@@ -1,11 +1,11 @@
 import { BandScale } from './scale-types';
 
 export function scaleBandInvert(scale: BandScale) {
-  var domain = scale.domain();
-  var paddingOuter = scale(domain[0]);
-  var eachBand = scale.step();
-  return function(value: number) {
-    var index = Math.floor((value - paddingOuter) / eachBand);
+  const domain = scale.domain();
+  const paddingOuter = scale(domain[0]);
+  const eachBand = scale.step();
+  return (value: number) => {
+    const index = Math.floor((value - paddingOuter) / eachBand);
     return domain[Math.max(0, Math.min(index, domain.length - 1))];
   };
 }
