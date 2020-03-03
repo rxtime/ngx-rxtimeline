8c2495e1ea72ed773a0a6bba72d54c2e8bb6b6f8
wflemingnz <wadefleming2000@hotmail.com>
2020-03-03 19:34:00 +0000

    Move functions out of OptionsService to orientation-utils (#181)
    * Remove TimeScale/BandScale conditional logic from axis service
    
    #101
    
    * Axis lines now render as expected
    
    * added typings
    
    * Move functions out of OptionsService to orientation-utils
    
    closes #180

--- projects/ngx-d3timeline/src/lib/options.service.ts
+++ projects/ngx-d3timeline/src/lib/options.service.ts
@@ -1,15 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Orientation } from './orientation';
 import { TimelineView } from './view/timeline-view';
-import { AxisOrientations } from './axis-orientations';
 
 @Injectable({ providedIn: 'root' })
 export class OptionsService {
-  setAxisOrientations(timeOrientation: Orientation): AxisOrientations {
-    const resourceOrientation = this.flipOrientation(timeOrientation);
-    return { time: timeOrientation, resource: resourceOrientation };
-  }
-
   getTranslation(
     range: number,
     orientation: Orientation,
@@ -18,12 +12,6 @@ export class OptionsService {
     return orientation === Orientation.Vertical
       ? `translate(${timelineView.left}, ${range})`
       : `translate(${range}, ${timelineView.top})`;
-  }
-
-  private flipOrientation(orientation: Orientation) {
-    return orientation === Orientation.Vertical
-      ? Orientation.Horizontal
-      : Orientation.Vertical;
   }
 
   getTickLineEnd(lineOffset: number, orientation: Orientation) {
