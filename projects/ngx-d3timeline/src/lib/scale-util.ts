c1d4da844f40df37f45d5b8f695d0ca6b11386f5
wflemingnz <wadefleming2000@hotmail.com>
2020-03-03 19:19:13 +0000

    move scale service to scale-utils (#179)
    * Remove TimeScale/BandScale conditional logic from axis service
    
    #101
    
    * Axis lines now render as expected
    
    * added typings
    
    * Move scaleService to scale-utils
    
    closes #175

--- projects/ngx-d3timeline/src/lib/scale-util.ts
+++ projects/ngx-d3timeline/src/lib/scale-util.ts
@@ -1,4 +1,9 @@
-import { BandScale } from './scale-types';
+import { TimeScale, BandScale } from './scale-types';
+import { Orientation } from './orientation';
+import { scaleBand, scaleTime } from 'd3-scale';
+import { TimelineEvent } from './timeline-event';
+import { min, max } from 'd3-array';
+import { TimelineView } from './view/timeline-view';
 
 export function scaleBandInvert(scale: BandScale) {
   const domain = scale.domain();
@@ -8,4 +13,54 @@ export function scaleBandInvert(scale: BandScale) {
     const index = Math.floor((value - paddingOuter) / eachBand);
     return domain[Math.max(0, Math.min(index, domain.length - 1))];
   };
+}
+
+export function rescaleTime(
+  data: TimelineEvent[],
+  view: TimelineView,
+  timeOrientation: Orientation,
+  event: any
+): TimeScale {
+  const unscaledTimeScale = configureTimeScale(data, view, timeOrientation);
+
+  return timeOrientation === Orientation.Vertical
+    ? event.transform.rescaleY(unscaledTimeScale)
+    : event.transform.rescaleX(unscaledTimeScale);
+}
+
+export function configureBandScale(
+  data: TimelineEvent[],
+  view: TimelineView,
+  orientation: Orientation
+): BandScale {
+  return scaleBand()
+    .domain(getBandScaleDomain(data))
+    .range(getRange(view, orientation));
+}
+
+export function configureTimeScale(
+  data: TimelineEvent[],
+  view: TimelineView,
+  orientation: Orientation
+): TimeScale {
+  return scaleTime()
+    .domain(getTimeScaleDomain(data))
+    .range(getRange(view, orientation));
+}
+
+function getBandScaleDomain(data: TimelineEvent[]): string[] {
+  return [...new Set(data.map(d => d.series))];
+}
+
+function getTimeScaleDomain(data: TimelineEvent[]): [Date, Date] {
+  return [min(data, d => d.start), max(data, d => d.finish)];
+}
+
+function getRange(
+  view: TimelineView,
+  orientation: Orientation
+): [number, number] {
+  return orientation === Orientation.Vertical
+    ? [view.top, view.bottom]
+    : [view.left, view.right];
 }
