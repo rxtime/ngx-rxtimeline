ee1518801438c4a082e5fee14c916613898eeace
Umar Mohammed <umohammed@caci.co.uk>
2020-03-04 19:47:46 +0000

    rename scaleBandInvert to InverseBandScale
    closes #196

--- projects/ngx-d3timeline/src/lib/drag-util.ts
+++ projects/ngx-d3timeline/src/lib/drag-util.ts
@@ -1,4 +1,4 @@
-import { scaleBandInvert } from './scale-util';
+import { getInverseBandScale } from './scale-util';
 import { State } from './store/state';
 import { Orientation } from './orientation';
 import { TimelineEvent } from '../public-api';
@@ -20,7 +20,7 @@ export function getDraggingTimelineEvent(state: State): TimelineEvent {
 }
 
 function getDropEventSeries(state: State) {
-  const invert = scaleBandInvert(state.bandScale);
+  const invert = getInverseBandScale(state.bandScale);
   return state.dragEvent && state.axisOrientations.time === Orientation.Vertical
     ? invert(state.dragEvent.x)
     : invert(state.dragEvent.y);
