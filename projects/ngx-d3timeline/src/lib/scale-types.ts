2b7289ffa58f63526ec11f1a118e6e64637305bd
umarmohammed <umar@thinkincode.co.uk>
2020-03-08 13:59:36 +0000

    rename scaleBandInvert to InverseBandScale (#197)
    closes #196
    
    Co-authored-by: Umar Mohammed <umohammed@caci.co.uk>
    Co-authored-by: Umar Mohammed <umar@teacherapp.co.uk>

--- projects/ngx-d3timeline/src/lib/scale-types.ts
+++ projects/ngx-d3timeline/src/lib/scale-types.ts
@@ -1,5 +1,6 @@
 import { ScaleBand, ScaleTime } from 'd3-scale';
 
 export type BandScale = ScaleBand<string>;
+export type InverseBandScale = (value: number) => string;
 export type TimeScale = ScaleTime<number, number>;
 export type Scale = BandScale | TimeScale;
