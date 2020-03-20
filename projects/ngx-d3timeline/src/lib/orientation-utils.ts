768c4632825100db10572f40b8a3e97a0c9f8710
Umar Mohammed <umohammed@caci.co.uk>
2020-03-20 19:42:28 +0000

    make options public api
    closes #295

--- projects/ngx-d3timeline/src/lib/orientation-utils.ts
+++ projects/ngx-d3timeline/src/lib/orientation-utils.ts
@@ -1,6 +1,11 @@
 import { Orientation } from './orientation';
+import { Options } from './options';
 
-export function flipOrientation(orientation: Orientation) {
+export function getTimeOrientation(options: Options): Orientation {
+  return options && Orientation[options.orientation];
+}
+
+export function flipOrientation(orientation: Orientation): Orientation {
   return orientation === Orientation.Vertical
     ? Orientation.Horizontal
     : Orientation.Vertical;
