014f92e2ca64248ff86b36b25768f5b569792908
Umar Mohammed <umohammed@caci.co.uk>
2020-03-25 01:11:58 +0000

    use viewClipRectangleHeight for rectangle height

--- projects/ngx-d3timeline/src/lib/resource-rectangle/resource-rectangle-utils.ts
+++ projects/ngx-d3timeline/src/lib/resource-rectangle/resource-rectangle-utils.ts
@@ -3,10 +3,6 @@ import { ResourceRectangle } from './resource-rectangle';
 import { pointToTransform } from '../core/point';
 import { View } from '../view/view';
 
-export function getResourceRectangleBreadth(view: View) {
-  return view.height - View.margin;
-}
-
 // TODO: Orientation #373
 export function getResourceRectangles(
   scale: BandScale,
--- projects/ngx-d3timeline/src/lib/resource-rectangle/resource-rectangle.selectors.ts
+++ projects/ngx-d3timeline/src/lib/resource-rectangle/resource-rectangle.selectors.ts
@@ -2,21 +2,14 @@ import { createSelector } from '../store-lib/selector/create-selector';
 import { selectBandScale } from '../scales/scale-selectors';
 import { selectView } from '../store/state';
 import { selectRectBreadthInResourceAxis } from '../activity-rectangle/selectors/activity-rectangle-size.selectors';
-import {
-  getResourceRectangleBreadth,
-  getResourceRectangles
-} from './resource-rectangle-utils';
-
-export const selectResourceRectangleBreadth = createSelector(
-  selectView,
-  getResourceRectangleBreadth
-);
+import { getResourceRectangles } from './resource-rectangle-utils';
+import { selectViewClipRectangleHeight } from '../view/view.selectors';
 
 // This could be better, tidy up as part of #373
 export const selectResourceRectangles = createSelector(
   selectBandScale,
   selectView,
   selectRectBreadthInResourceAxis,
-  selectResourceRectangleBreadth,
+  selectViewClipRectangleHeight,
   getResourceRectangles
 );
