fdad63cec6072de5b1dcc8f403844f4d9bafbd34
Umar Mohammed <umohammed@caci.co.uk>
2020-03-27 15:40:15 +0000

    delete content utils

--- projects/ngx-d3timeline/src/lib/content/content-utils.ts
+++
@@ -1,6 +0,0 @@
-import { PositionedActivity } from '../activity/positioned-activity';
-import { TimeScale, BandScale } from '../scales/scale-types';
-import { Orientation } from '../core/orientation';
-import { Point, translatePoint, pointToTransform } from '../core/point';
-import { ActivityRectangle } from '../activity-rectangle/activity-rectangle';
-import { getTextWidth } from '../core/text-utils';
