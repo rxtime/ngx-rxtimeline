aa0b5671781b7e12c558db0b7546ab64e5f9425d
Umar Mohammed <umohammed@caci.co.uk>
2020-03-28 12:47:55 +0000

    refactor options utils
    closes #436

--- projects/ngx-d3timeline/src/lib/options/selectors/options.selectors.ts
+++ projects/ngx-d3timeline/src/lib/options/selectors/options.selectors.ts
@@ -1,12 +1,17 @@
 import { createSelector } from '../../store-lib/selector/create-selector';
 import { selectOptions } from '../../store/state';
-import { flipOrientation } from '../../core/orientation';
-import { getTimeOrientation } from '../options-utils';
+import { flipOrientation, Orientation } from '../../core/orientation';
+import { Options } from '../options';
 
 export const selectTimeOrientation = createSelector(
   selectOptions,
   getTimeOrientation
 );
+
+function getTimeOrientation(options: Options): Orientation {
+  return options && Orientation[options.orientation];
+}
+
 export const selectResourceOrientation = createSelector(
   selectTimeOrientation,
   flipOrientation
--- projects/ngx-d3timeline/src/lib/options/selectors/type-options.selectors.ts
+++ projects/ngx-d3timeline/src/lib/options/selectors/type-options.selectors.ts
@@ -1,7 +1,11 @@
 import { createSelector } from '../../store-lib/selector/create-selector';
 import { selectOptions } from '../../store/state';
-import { getTypeOptions } from '../options-utils';
+import { CompleteOptions, TypeOptions } from '../options';
 
 export const selectTypeOptions = createSelector(selectOptions, options =>
   getTypeOptions.bind(null, options)
 );
+
+function getTypeOptions(options: CompleteOptions, type: string): TypeOptions {
+  return options.type && options.type[type];
+}
