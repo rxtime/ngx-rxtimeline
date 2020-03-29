0e67167612ad84ebf68f0bb50238cf539a1ca1b5
Umar Mohammed <umohammed@caci.co.uk>
2020-03-29 18:50:49 +0100

    handle orientation for resource rectangles
    closes #373

---
+++ projects/ngx-d3timeline/src/lib/resource-rectangle/resource-rectangle-size.selectors.ts
@@ -0,0 +1,67 @@
+import { createSelector } from '../store-lib/selector/create-selector';
+import { selectBandScaleWidth } from '../scales/scale-selectors';
+import {
+  selectViewClipRectangleHeight,
+  selectViewClipRectangleWidth
+} from '../view/view.selectors';
+import { selectTimeOrientation } from '../options/selectors/options.selectors';
+import { Orientation } from '../core/orientation';
+import { sum } from '../core/array-utils';
+import { selectTimeAxisOffset } from './resource-rectangle-position.selectors';
+
+const selectClipRectBreadthInTimeAxis = createSelector(
+  selectViewClipRectangleHeight,
+  selectViewClipRectangleWidth,
+  selectTimeOrientation,
+  getClipRectBreadthInTimeAxis
+);
+
+function getClipRectBreadthInTimeAxis(
+  clipRectangleHeight: number,
+  clipRectangleWidth: number,
+  timeOrientation: Orientation
+) {
+  return timeOrientation === Orientation.Vertical
+    ? clipRectangleHeight
+    : clipRectangleWidth;
+}
+
+const selectRectBreadthInTimeAxis = createSelector(
+  selectTimeAxisOffset,
+  selectClipRectBreadthInTimeAxis,
+  sum
+);
+
+export const selectRectWidth = createSelector(
+  selectTimeOrientation,
+  selectRectBreadthInTimeAxis,
+  selectBandScaleWidth,
+  getRectWidth
+);
+
+function getRectWidth(
+  timeOrientation: Orientation,
+  rectBreadthInTimeAxis: number,
+  bandScaleWidth: number
+) {
+  return timeOrientation === Orientation.Vertical
+    ? bandScaleWidth
+    : rectBreadthInTimeAxis;
+}
+
+export const selectRectHeight = createSelector(
+  selectTimeOrientation,
+  selectRectBreadthInTimeAxis,
+  selectBandScaleWidth,
+  getRectHeight
+);
+
+function getRectHeight(
+  timeOrientation: Orientation,
+  rectBreadthInTimeAxis: number,
+  bandScaleWidth: number
+) {
+  return timeOrientation === Orientation.Vertical
+    ? rectBreadthInTimeAxis
+    : bandScaleWidth;
+}
