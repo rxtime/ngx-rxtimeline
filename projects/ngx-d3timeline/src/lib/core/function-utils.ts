d18fbca19ca36202b0e70fa0ebca7c30c2254eb1
wflemingnz <wadefleming2000@hotmail.com>
2020-03-29 21:45:09 +0100

    remove func.call in partialApply (#475)
    closes #474
    
    Co-authored-by: Wade Fleming <WFleming@caci.co.uk>

--- projects/ngx-d3timeline/src/lib/core/function-utils.ts
+++ projects/ngx-d3timeline/src/lib/core/function-utils.ts
@@ -2,5 +2,5 @@ export function partialApply<TInput, TAppliedArgs extends any[], TOut>(
   func: (input: TInput, ...appliedArgs: TAppliedArgs) => TOut
 ): (...appliedArgs: TAppliedArgs) => (input: TInput) => TOut {
   return (...appliedArgs: TAppliedArgs) => (input: TInput) =>
-    func.call(null, input, ...appliedArgs);
+    func(input, ...appliedArgs);
 }
