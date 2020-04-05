a47df1c59bc0589720587fcd48ca87d76f7d8f1c
Wade Fleming <WFleming@caci.co.uk>
2020-04-05 15:51:36 +0100

    refactor createResizeObservable to use nested functions

--- projects/ngx-d3timeline/src/lib/core/resize-observable.ts
+++ projects/ngx-d3timeline/src/lib/core/resize-observable.ts
@@ -4,24 +4,24 @@ declare var ResizeObserver: any; // typings not yet available in Typescript
 declare type ResizeObserver = any;
 
 export function createResizeObservable(element: Element) {
+  let resizeObserver: ResizeObserver;
+
   return fromEventPattern<[number, number]>(
-    handler => addResizeHandler(handler, element),
-    (handler, token) => removeResizeHandler(handler, token)
+    handler => addResizeHandler(handler),
+    handler => removeResizeHandler(handler)
   );
-}
 
-function addResizeHandler(
-  handler: ([width, height]: [number, number]) => void,
-  element: Element
-) {
-  const resizeObserver = new ResizeObserver(entries => {
-    const entry = entries[0];
-    handler([entry.contentRect.width, entry.contentRect.height]);
-  });
-  resizeObserver.observe(element);
-  return resizeObserver;
-}
+  function addResizeHandler(
+    handler: ([width, height]: [number, number]) => void
+  ) {
+    resizeObserver = new ResizeObserver(entries => {
+      const entry = entries[0];
+      handler([entry.contentRect.width, entry.contentRect.height]);
+    });
+    resizeObserver.observe(element);
+  }
 
-function removeResizeHandler(_: any, token: ResizeObserver) {
-  token.disconnect();
+  function removeResizeHandler(_: any) {
+    resizeObserver.unobserve(element);
+  }
 }
