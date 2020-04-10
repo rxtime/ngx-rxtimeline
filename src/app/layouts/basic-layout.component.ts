defe2ba6843477cec06f6fd5de1069d9daca0efb
Umar Mohammed <umar@teacherapp.co.uk>
2020-04-10 15:01:15 +0100

    use div wrapper in basic layout
    closes #517

--- src/app/layouts/basic-layout.component.ts
+++ src/app/layouts/basic-layout.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   template: `
-    <app-timeline></app-timeline>
+    <div class="timeline-container">
+      <app-timeline></app-timeline>
+    </div>
     <app-options></app-options>
   `,
-  selector: 'app-basic-layout'
+  selector: 'app-basic-layout',
+  styles: [
+    `
+      .timeline-container {
+        width: 800px;
+        height: 800px;
+      }
+    `
+  ]
 })
 export class BasicLayoutComponent {}
