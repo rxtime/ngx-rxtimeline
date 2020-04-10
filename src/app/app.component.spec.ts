d66591adb4e09af530144add500f5cec2617e012
Umar Mohammed <umar@teacherapp.co.uk>
2020-04-10 14:06:07 +0100

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 import { NgxRxTimelineModule } from '../../projects/ngx-rxtimeline/src/public-api';
 import { FormsModule } from '@angular/forms';
+import { OptionsComponent } from './options/options.component';
+import { CssGridLayoutComponent } from './layouts/css-grid-layout.component';
+import { BasicLayoutComponent } from './layouts/basic-layout.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      imports: [NgxRxTimelineModule, FormsModule]
+      declarations: [
+        AppComponent,
+        OptionsComponent,
+        CssGridLayoutComponent,
+        BasicLayoutComponent,
+        TimelineComponent
+      ],
+      imports: [NgxRxTimelineModule, FormsModule, RouterTestingModule]
     }).compileComponents();
   }));
 
